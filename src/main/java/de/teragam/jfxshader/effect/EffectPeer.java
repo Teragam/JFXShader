f55736221186ce438fc5718d2570e01bb02f2a31
Nico Paradies <nico.paradies@uni-oldenburg.de>
2023-07-27 17:40:17 +0200

    Improved Javadoc for EffectPeer

--- src/main/java/de/teragam/jfxshader/effect/EffectPeer.java
+++ src/main/java/de/teragam/jfxshader/effect/EffectPeer.java
@@ -17,18 +17,18 @@ import de.teragam.jfxshader.ImagePoolPolicy;
 public @interface EffectPeer {
 
     /**
-     * The unique name of the peer.
+     * @return The unique name of the peer.
      */
     String value();
 
     /**
-     * The pixel format of the target texture.
+     * @return The pixel format of the target texture.
      * Defaults to {@link PixelFormat#INT_ARGB_PRE}.
      */
     PixelFormat targetFormat() default PixelFormat.INT_ARGB_PRE;
 
     /**
-     * The wrap mode of the target texture.
+     * @return The wrap mode of the target texture.
      * Defaults to {@link Texture.WrapMode#CLAMP_TO_ZERO}.
      */
     Texture.WrapMode targetWrapMode() default Texture.WrapMode.CLAMP_TO_ZERO;
@@ -39,19 +39,20 @@ public @interface EffectPeer {
      * <p>
      * <b>Note:</b> For Opengl, due to the use of deprecated functions, mipmaps are only generated if the
      * version of OpenGL is below 3.1.
+     *
+     * @return Whether mipmaps should be generated for the target texture.
      */
     boolean targetMipmaps() default false;
 
     /**
-     * The image pool policy for the target texture.
+     * @return The image pool policy for the target texture.
      * Defaults to {@link ImagePoolPolicy#LENIENT}.
-     *
      * @see ImagePoolPolicy
      */
     ImagePoolPolicy targetPoolPolicy() default ImagePoolPolicy.LENIENT;
 
     /**
-     * Whether the peer is a singleton and should be reused for all instances of the effect.
+     * @return Whether the peer is a singleton and should be reused for all instances of the effect.
      * Defaults to {@code true}.
      */
     boolean singleton() default true;
