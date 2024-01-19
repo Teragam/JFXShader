980608b6c802e0e81447026f2becf64f4e8ad7e3
Nico Paradies <nico.paradies@uni-oldenburg.de>
2024-01-19 14:39:15 +0100

    Increased version

--- src/main/java/de/teragam/jfxshader/JFXShaderModule.java
+++ src/main/java/de/teragam/jfxshader/JFXShaderModule.java
@@ -8,7 +8,7 @@ import de.teragam.jfxshader.util.Reflect;
 
 public final class JFXShaderModule {
 
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.3.1";
 
     private static final AtomicBoolean initialized = new AtomicBoolean();
 
@@ -47,7 +47,7 @@ public final class JFXShaderModule {
                 "com.sun.scenario.effect.impl.state", "com.sun.scenario.effect", "com.sun.prism", "com.sun.prism.impl", "com.sun.scenario.effect.impl.prism",
                 "com.sun.prism.ps", "com.sun.glass.ui", "com.sun.glass.utils", "com.sun.javafx.scene.shape", "com.sun.javafx.sg.prism", "com.sun.javafx.util",
                 "com.sun.prism.impl.ps", "com.sun.scenario.effect.impl", "com.sun.scenario.effect.impl.prism.ps", "com.sun.javafx.scene.paint",
-                "com.sun.javafx.tk", "com.sun.javafx.beans.event", "javafx.scene.effect", "javafx.scene.paint");
+                "com.sun.javafx.tk", "com.sun.javafx.beans.event", "javafx.scene.effect", "javafx.scene.paint", "javafx.scene");
     }
 
     private static List<String> getOptionalGraphicsPackages() {
