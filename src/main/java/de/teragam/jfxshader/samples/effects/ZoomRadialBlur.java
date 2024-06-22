ff6e12bc2a7e6bd2fac240be7d11b8af231808dd
Nico Paradies <nico.paradies@uni-oldenburg.de>
2024-06-22 12:25:49 +0200

    Fixed dirty regions for ZoomRadialBlur

--- src/main/java/de/teragam/jfxshader/samples/effects/ZoomRadialBlur.java
+++ src/main/java/de/teragam/jfxshader/samples/effects/ZoomRadialBlur.java
@@ -3,8 +3,20 @@ package de.teragam.jfxshader.samples.effects;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
 
+import com.sun.javafx.geom.BaseBounds;
+import com.sun.javafx.geom.DirtyRegionContainer;
+import com.sun.javafx.geom.RectBounds;
+import com.sun.javafx.geom.Rectangle;
+import com.sun.javafx.geom.transform.BaseTransform;
+import com.sun.scenario.effect.Effect;
+import com.sun.scenario.effect.FilterContext;
+import com.sun.scenario.effect.ImageData;
+import com.sun.scenario.effect.impl.prism.PrRenderInfo;
+import com.sun.scenario.effect.impl.state.RenderState;
+
 import de.teragam.jfxshader.effect.EffectDependencies;
 import de.teragam.jfxshader.effect.OneSamplerEffect;
+import de.teragam.jfxshader.effect.ShaderEffect;
 
 @EffectDependencies(ZoomRadialBlurEffectPeer.class)
 public class ZoomRadialBlur extends OneSamplerEffect {
@@ -27,6 +39,84 @@ public class ZoomRadialBlur extends OneSamplerEffect {
         this.blurSteps = super.createEffectIntegerProperty(blurSteps, "blurSteps");
         this.centerX = super.createEffectDoubleProperty(0.0, "centerX");
         this.centerY = super.createEffectDoubleProperty(0.0, "centerY");
+    }
+
+    @Override
+    public RenderState getRenderState(FilterContext fctx, BaseTransform transform, Rectangle outputClip, PrRenderInfo renderHelper, Effect defaultInput) {
+        return new RenderState() {
+            @Override
+            public EffectCoordinateSpace getEffectTransformSpace() {
+                return EffectCoordinateSpace.CustomSpace;
+            }
+
+            @Override
+            public BaseTransform getInputTransform(BaseTransform baseTransform) {
+                return baseTransform;
+            }
+
+            @Override
+            public BaseTransform getResultTransform(BaseTransform baseTransform) {
+                return BaseTransform.IDENTITY_TRANSFORM;
+            }
+
+            @Override
+            public Rectangle getInputClip(int i, Rectangle rectangle) {
+                final BaseBounds inputBounds = ZoomRadialBlur.super.getInputBounds(0, BaseTransform.IDENTITY_TRANSFORM, defaultInput);
+                final Rectangle untransformedClip = ShaderEffect.untransformClip(transform, outputClip);
+                final BaseBounds bounds = ZoomRadialBlur.this.calcInfluenceBounds(inputBounds, new RectBounds(untransformedClip));
+                transform.transform(bounds, bounds);
+                bounds.roundOut();
+                return new Rectangle((int) bounds.getMinX(), (int) bounds.getMinY(), (int) bounds.getWidth(), (int) bounds.getHeight());
+            }
+        };
+    }
+
+    @Override
+    public BaseBounds getBounds(BaseBounds inputBounds) {
+        return inputBounds.deriveWithNewBounds(this.calcBounds(inputBounds, inputBounds));
+    }
+
+    @Override
+    public Rectangle getResultBounds(BaseTransform transform, Rectangle outputClip, ImageData... inputDatas) {
+        return outputClip;
+    }
+
+    @Override
+    public void writeDirtyRegions(BaseBounds inputBounds, DirtyRegionContainer drc) {
+        for (int i = 0; i < drc.size(); i++) {
+            final RectBounds region = drc.getDirtyRegion(i);
+            if (region == null || region.isEmpty()) {
+                break;
+            }
+            region.deriveWithNewBounds(this.calcBounds(inputBounds, region));
+        }
+    }
+
+    private RectBounds calcBounds(BaseBounds inputBounds, BaseBounds region) {
+        final double calcStrength = this.strength.get() * (1.0 / Math.max(inputBounds.getWidth(), inputBounds.getHeight()));
+        final float maxOffset = (float) (calcStrength * (this.getBlurSteps() - 1) / this.getBlurSteps());
+        final float minXOffset = (float) ((region.getMinX() - this.getCenterX() - inputBounds.getMinX()) * maxOffset);
+        final float minYOffset = (float) ((region.getMinY() - this.getCenterY() - inputBounds.getMinY()) * maxOffset);
+        final float maxXOffset = (float) ((region.getMaxX() - this.getCenterX() - inputBounds.getMinX()) * maxOffset);
+        final float maxYOffset = (float) ((region.getMaxY() - this.getCenterY() - inputBounds.getMinY()) * maxOffset);
+        final float newMinX = (float) (region.getMinX() + Math.min(minXOffset / (1.0 - maxOffset), 0));
+        final float newMinY = (float) (region.getMinY() + Math.min(minYOffset / (1.0 - maxOffset), 0));
+        final float newMaxX = (float) (region.getMaxX() + Math.max(maxXOffset / (1.0 - maxOffset), 0));
+        final float newMaxY = (float) (region.getMaxY() + Math.max(maxYOffset / (1.0 - maxOffset), 0));
+        return new RectBounds(newMinX, newMinY, newMaxX, newMaxY);
+    }
+
+    private RectBounds calcInfluenceBounds(BaseBounds inputBounds, BaseBounds region) {
+        final double calcStrength = this.strength.get() * (1.0 / Math.max(inputBounds.getWidth(), inputBounds.getHeight()));
+        final float maxOffset = (float) (calcStrength * (this.getBlurSteps() - 1) / this.getBlurSteps());
+        final float newMinX = (float) (region.getMinX() * (-maxOffset) + region.getMinX() + maxOffset * (this.getCenterX() + inputBounds.getMinX()));
+        final float newMinY = (float) (region.getMinY() * (-maxOffset) + region.getMinY() + maxOffset * (this.getCenterY() + inputBounds.getMinY()));
+        final float newMaxX = (float) (region.getMaxX() * (-maxOffset) + region.getMaxX() + maxOffset * (this.getCenterX() + inputBounds.getMinX()));
+        final float newMaxY = (float) (region.getMaxY() * (-maxOffset) + region.getMaxY() + maxOffset * (this.getCenterY() + inputBounds.getMinY()));
+        final BaseBounds result = new RectBounds(newMinX, newMinY, newMaxX, newMaxY).deriveWithUnion(region);
+        final BaseBounds totalRegion = this.calcBounds(inputBounds, inputBounds);
+        result.intersectWith(totalRegion);
+        return result.flattenInto(null);
     }
 
     public double getStrength() {
--- src/main/java/de/teragam/jfxshader/samples/effects/ZoomRadialBlurEffectPeer.java
+++ src/main/java/de/teragam/jfxshader/samples/effects/ZoomRadialBlurEffectPeer.java
@@ -2,6 +2,10 @@ package de.teragam.jfxshader.samples.effects;
 
 import java.util.Map;
 
+import com.sun.javafx.geom.BaseBounds;
+import com.sun.javafx.geom.Rectangle;
+import com.sun.javafx.geom.transform.NoninvertibleTransformException;
+
 import de.teragam.jfxshader.JFXShader;
 import de.teragam.jfxshader.ShaderDeclaration;
 import de.teragam.jfxshader.effect.EffectPeer;
@@ -18,7 +22,7 @@ class ZoomRadialBlurEffectPeer extends ShaderEffectPeer<ZoomRadialBlur> {
     @Override
     protected ShaderDeclaration createShaderDeclaration() {
         final Map<String, Integer> samplers = Map.of("baseImg", 0);
-        final Map<String, Integer> params = Map.of("resolution", 0, "center", 1, "viewport", 2, "texCoords", 3, "blurSteps", 4, "strength", 5);
+        final Map<String, Integer> params = Map.of("center", 0, "blurSteps", 1, "strength", 2);
         return new ShaderDeclaration(samplers, params,
                 ZoomRadialBlurEffectPeer.class.getResourceAsStream("/de/teragam/jfxshader/samples/effects/zoomradialblur/zoomradialblur.frag"),
                 ZoomRadialBlurEffectPeer.class.getResourceAsStream("/de/teragam/jfxshader/samples/effects/zoomradialblur/zoomradialblur.obj"));
@@ -26,15 +30,25 @@ class ZoomRadialBlurEffectPeer extends ShaderEffectPeer<ZoomRadialBlur> {
 
     @Override
     protected void updateShader(JFXShader shader, ZoomRadialBlur effect) {
-        shader.setConstant("resolution", (float) this.getDestBounds().width, (float) this.getDestBounds().height);
-        shader.setConstant("center", (float) effect.getCenterX(), (float) effect.getCenterY());
+        final BaseBounds contentBounds = super.getInputEffectBounds(0);
+        final Rectangle bounds = this.getInputBounds(0);
+        final Rectangle inputNativeBounds = this.getInputNativeBounds(0);
         final double scaleX = Math.hypot(this.getTransform().getMxx(), this.getTransform().getMyx());
         final double scaleY = Math.hypot(this.getTransform().getMxy(), this.getTransform().getMyy());
-        shader.setConstant("viewport", 0f, 0f, (float) scaleX, (float) scaleY);
-        final float[] texCoords = this.getTextureCoords(0);
-        shader.setConstant("texCoords", texCoords[0], texCoords[1], texCoords[2], texCoords[3]);
+        final float centerX = (float) (effect.getCenterX() * scaleX - (bounds.x - contentBounds.getMinX()));
+        final float centerY = (float) (effect.getCenterY() * scaleY - (bounds.y - contentBounds.getMinY()));
+        final float[] center = new float[]{centerX, centerY};
+        try {
+            super.getInputTransform(0).inverseDeltaTransform(center, 0, center, 0, 1);
+        } catch (NoninvertibleTransformException e) {
+            // Ignore
+        }
+        center[0] /= inputNativeBounds.width;
+        center[1] /= inputNativeBounds.height;
+        shader.setConstant("center", center[0], center[1]);
         shader.setConstant("blurSteps", Math.max(Math.min(effect.getBlurSteps(), 64), 1));
-        shader.setConstant("strength", (float) effect.getStrength());
+        final double strength = effect.getStrength() * (1.0 / Math.max(contentBounds.getWidth(), contentBounds.getHeight())) * Math.max(scaleX, scaleY);
+        shader.setConstant("strength", (float) (strength));
     }
 
 }
