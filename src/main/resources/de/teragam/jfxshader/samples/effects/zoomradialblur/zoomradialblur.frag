3b3a0867dfd4d43c21d167f16f853a3381a36bb9
Nico Paradies <nico.paradies@uni-oldenburg.de>
2024-06-22 12:25:49 +0200

    Fixed dirty regions for ZoomRadialBlur

--- src/main/resources/de/teragam/jfxshader/samples/effects/zoomradialblur/zoomradialblur.frag
+++ src/main/resources/de/teragam/jfxshader/samples/effects/zoomradialblur/zoomradialblur.frag
@@ -1,29 +1,18 @@
 varying vec2 texCoord0;
 
 uniform sampler2D baseImg;
-uniform vec2 resolution;
 uniform vec2 center;
-uniform vec4 viewport;
-uniform vec4 texCoords;
 uniform int blurSteps;
 uniform float strength;
 
 void main() {
-    vec2 scaledCenter = ((center / resolution + texCoords.xy) * texCoords.zw) * viewport.zw;
-    vec2 focus = texCoord0 - scaledCenter;
-    float calcStrength = strength * (1.0 / max(resolution.x, resolution.y)) * viewport.z;
-
+    vec2 focus = texCoord0 - center;
     vec4 outColor = vec4(0.0, 0.0, 0.0, 0.0);
-
     for (int i = 0; i < blurSteps; i++) {
-        float power = 1.0 - calcStrength * float(i) / float(blurSteps);
-        vec2 texCoord = focus * power + scaledCenter;
-        if (texCoord.x < texCoords.x || texCoord.y < texCoords.y || texCoord.x > texCoords.z || texCoord.y > texCoords.w) {
-            break;
-        }
+        float power = 1.0 - strength * float(i) / float(blurSteps);
+        vec2 texCoord = focus * power + center;
         outColor += texture2D(baseImg, texCoord);
     }
-
     outColor *= 1.0 / float(blurSteps);
     gl_FragColor = outColor;
 }
--- src/main/resources/de/teragam/jfxshader/samples/effects/zoomradialblur/zoomradialblur.hlsl
+++ src/main/resources/de/teragam/jfxshader/samples/effects/zoomradialblur/zoomradialblur.hlsl
@@ -1,33 +1,21 @@
 sampler2D baseImg : register(s0);
 
-float2 resolution : register(c0);
-float2 center : register(c1);
-float4 viewport : register(c2);
-float4 texCoords : register(c3);
-int blurSteps : register(c4);
-float strength : register(c5);
+float2 center : register(c0);
+int blurSteps : register(c1);
+float strength : register(c2);
 
 void main(float2 pos0 : TEXCOORD0, float2 pos1 : TEXCOORD1, float2 pixcoord : VPOS, float4 jsl_vertexColor : COLOR0, out float4 color : SV_Target) {
-
-    float2 scaledCenter = ((center / resolution + texCoords.xy) * texCoords.zw) * viewport.zw;
-    float2 focus = pos0 - scaledCenter;
-    float calcStrength = strength * (1.0 / max(resolution.x, resolution.y)) * viewport.z;
-
+    float2 focus = pos0 - center;
     float4 outColor = float4(0.0, 0.0, 0.0, 0.0);
-
     [unroll]
     for (int i = 0; i < 64; i++) {
         if (i >= blurSteps) {
             break;
         }
-        float power = 1.0 - calcStrength * float(i) / float(blurSteps);
-        float2 texCoord = focus * power + scaledCenter;
-        if (texCoord.x < texCoords.x || texCoord.y < texCoords.y || texCoord.x > texCoords.z || texCoord.y > texCoords.w) {
-            break;
-        }
+        float power = 1.0 - strength * float(i) / float(blurSteps);
+        float2 texCoord = focus * power + center;
         outColor += tex2D(baseImg, texCoord);
     }
-
     outColor *= 1.0 / float(blurSteps);
     color = outColor;
 }
