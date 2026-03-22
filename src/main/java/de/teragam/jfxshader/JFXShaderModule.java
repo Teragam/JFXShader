57a8d71f21ba62bf1c4ad571cad464f1b1aeb230
Nico Paradies <paradiesnico@gmail.com>
2026-03-22 17:15:00 +0100

    Increased version to 1.4.0

--- src/main/java/de/teragam/jfxshader/JFXShaderModule.java
+++ src/main/java/de/teragam/jfxshader/JFXShaderModule.java
@@ -8,7 +8,7 @@ import de.teragam.jfxshader.util.Reflect;
 
 public final class JFXShaderModule {
 
-    public static final String VERSION = "1.4.0-SNAPSHOT";
+    public static final String VERSION = "1.4.0";
 
     private static final AtomicBoolean INITIALIZED = new AtomicBoolean();
 
